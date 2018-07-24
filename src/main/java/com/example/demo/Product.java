3e5daaae27ef9720caf923e479bbcffd578cfb70
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-07-24 16:53:07 +0200

    cos juz jest

--- src/main/java/com/example/demo/Product.java
+++ src/main/java/com/example/demo/Product.java
@@ -1,16 +1,87 @@
 package com.example.demo;
 
 public class Product {
+private int ilosc;
+private String name;
+private String producer;
+private int serianNr;
+private String imgUrl;
+private double price;
 
-String name;
-String producer;
-int serianNr;
-double price;
+    public Product() {
+    }
 
-    public Product(String name, String producer, int serianNr, double price) {
+    public Product(String name, String producer,String img, int serianNr, double price) {
         this.name = name;
         this.producer = producer;
         this.serianNr = serianNr;
+        this.price = price;
+        this.imgUrl = img;
+
+    }
+
+    public Product(int ilosc, String name, String producer, int serianNr, String imgUrl, double price) {
+        this.ilosc = ilosc;
+        this.name = name;
+        this.producer = producer;
+        this.serianNr = serianNr;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    }
+
+    public int getIlosc() {
+        return ilosc;
+    }
+
+    public void setIlosc(int ilosc) {
+        this.ilosc = ilosc;
+    }
+
+    public String getImg() {
+        return imgUrl;
+    }
+
+    public void setImg(String img) {
+        this.imgUrl = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getSerianNr() {
+        return serianNr;
+    }
+
+    public void setSerianNr(int serianNr) {
+        this.serianNr = serianNr;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }
--- src/main/java/com/example/demo/ShoppingCart.java
+++ src/main/java/com/example/demo/ShoppingCart.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ShoppingCart {
 
+//    @GetMapping(@RequestParam ilosc"/addToCart")
+//    public String addProductForm(Model model) {
+//        model.addAttribute("product", new Product());
+//        return "addProduct";
+//
+    }
 
-
-}
