7ae6157bf0256bb668696930b2f0dcf6d4a656e0
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-08-06 21:51:43 +0200

    dziala

--- src/main/java/com/example/demo/ShopController.java
+++ src/main/java/com/example/demo/ShopController.java
@@ -3,15 +3,21 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+
 @Controller
 public class ShopController {
 
     private ProductRepository productRepository;
+    private ShoppingCart shoppingCart;
 
-    public ShopController(ProductRepository productRepository) {
+    public ShopController(ProductRepository productRepository, ShoppingCart shoppingCart) {
         this.productRepository = productRepository;
+        this.shoppingCart = shoppingCart;
     }
 
     @GetMapping("/")
@@ -22,5 +28,23 @@ public class ShopController {
         return "home";
     }
 
+    @PostMapping("/addbasket")
+    public String basket(@RequestParam String name, @RequestParam int ilosc) {
+        shoppingCart.addProduct(name, ilosc);
+        return "add";
+    }
 
+    @ResponseBody
+    @GetMapping("/basket")
+    public String showBasket() {
+        return shoppingCart.toString();
+    }
+
+
+    @ResponseBody
+    @GetMapping("/sum")
+    public String showSum() {
+        return "Do zapłaty " + shoppingCart.sumProduct() + " zł"
+                + "<br/><br/><a href=\"/\">Kontynuuj zakupy</a><br/>";
+    }
 }
--- src/main/java/com/example/demo/ShoppingCart.java
+++ src/main/java/com/example/demo/ShoppingCart.java
@@ -1,17 +1,59 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class ShoppingCart {
 
-//    @GetMapping(@RequestParam ilosc"/addToCart")
-//    public String addProductForm(Model model) {
-//        model.addAttribute("product", new Product());
-//        return "addProduct";
-//
+    public Map<String, Integer> basketMap;
+    private ProductRepository productRepository;
+
+    public ShoppingCart() {
+        this.basketMap = new HashMap<>();
+        this.productRepository = new ProductRepository();
     }
+
+    public void addProduct(String name, Integer ilosc) {
+
+        if (basketMap.isEmpty()) {
+            basketMap.put(name, ilosc);
+
+        } else {
+            if (isItem(name, ilosc)) {
+                basketMap.put(name, ilosc + basketMap.get(name));
+            } else {
+                basketMap.put(name, ilosc);
+            }
+        }
+    }
+
+    public boolean isItem(String name, Integer ilosc) {
+        for (Map.Entry<String, Integer> entry : basketMap.entrySet()) {
+            if (entry.getKey().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+
+
+    @Override
+    public String toString() {
+        return basketMap + "<br/><br/><a href=\"/\">Kontynuuj zakupy</a><br/>";
+    }
+
+    public double sumProduct() {
+        double sum = 0;
+        for (Map.Entry<String, Integer> entry : basketMap.entrySet()) {
+            sum += entry.getValue() * productRepository.getPriceProductName(entry.getKey());
+        }
+        return sum;
+    }
+}
 
